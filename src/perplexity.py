c08c0e8b209882f4bcb047d49a0b1cd67ba3160a
Soumen Mondal <mondalsoumen00@gmail.com>
2024-08-06 00:59:23 +0530

    Add perplexity matrix

--- src/perplexity.py
+++ src/perplexity.py
@@ -9,25 +9,42 @@ from dataset import WikipediaDataset
 from models import LlamaModelForProbing
       
 class Perplexity:
-    def __init__(self, device: torch.device, model: torch.nn.Module, model_name: str, src_lang_dataset: Dataset):
+    def __init__(self, device: torch.device, tokenizer: AutoTokenizer, model: torch.nn.Module, model_name: str, ppx_config: dict):
         self.device = device
-        self.src_lang_dataset = src_lang_dataset
-        self.src_lang = self.src_lang_dataset.lang
+        self.tokenizer = tokenizer
         self.model_name = model_name.split("/")[-1]
         self.model = model
+        self.batch_size = ppx_config["batch_size"]
+        self.data_frac = ppx_config["data_frac"]
+        self.Tmax = ppx_config["max_context_len"]
+        self.lang_list = ppx_config["lang_list"]
+        
         self.cwd = Path.cwd()
         self.lang_neuron_path = Path(self.cwd, f"outputs/lang_neurons/{self.model_name}/lang_neuron_data.pkl")
         self.lang_neuron = pickle.load(open(self.lang_neuron_path, "rb"))
     
     def info(self) -> str:
         return f"[INFO] {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"
-    
-    def _calc_ppx(self, dl: DataLoader, lang: str, lang_type: str, intervene_config: Union[dict, None]):
+
+    def _calc_ppx_for_lang(self, dataset: Dataset, inv_lang: Union[str, None], lang: str) -> float:
+        """dataset: for which we want to calculate the perplexity
+        inv_lang: where we want to intervene the activation (inv_lang specific neurons), can be None if no intervention is needed
+        lang: for which we want to calculate the perplexity score
+        """
+        dl = dataset.prepare_dataloader(batch_size=self.batch_size, frac=self.data_frac)
+        if inv_lang is None:
+            desc = f"Calculating perplexity without intervention for lang: {lang}"
+            intervene_config = None
+        else:
+            # lang specific neurons will be deactivated here
+            desc = f"Calculating perplexity with intervention in lang: {inv_lang} for lang: {lang}"
+            intervene_config = {
+                "indices": self.lang_neuron["lang_to_neuron"][inv_lang],
+                "value": 0
+            }
+        
         loss_list = []
-        with tqdm.tqdm(iterable=dl, 
-                       desc=f"Calculating perplexity for {lang_type} lang: {lang}",
-                       unit=" batches",
-                       colour="green") as pbar:
+        with tqdm.tqdm(iterable=dl, desc=desc, unit=" batches", colour="green") as pbar:
             for input_dict in pbar:
                 out_dict = self.model(input_dict["input_ids"], input_dict["attention_mask"], intervene_config=intervene_config)
                 logits = out_dict["logits"] # (b, Tmax, V) 
@@ -43,21 +60,25 @@ class Perplexity:
         ppx = math.exp(avg_loss)
         return ppx
     
-    def _calc_ppx_for_src_lang(self, batch_size: int, data_frac: float) -> float:
-        dl = self.src_lang_dataset.prepare_dataloader(batch_size=batch_size, frac=data_frac)
-        ppx = self._calc_ppx(dl, lang=self.src_lang, lang_type="source", intervene_config=None)
-        return ppx
-
-    def _calc_ppx_for_tgt_lang(self, tgt_lang_dataset: str, batch_size: int, data_frac: float) -> float:
-        """src lang neurons will be deactivated here"""
-        tgt_lang = tgt_lang_dataset.lang
-        tgt_dl = tgt_lang_dataset.prepare_dataloader(batch_size=batch_size, frac=data_frac)
-        intervene_config = {
-            "indices": self.lang_neuron["lang_to_neuron"][self.src_lang],
-            "value": 0
-        }
-        ppx = self._calc_ppx(tgt_dl, lang=tgt_lang, lang_type="target", intervene_config=intervene_config)
-        return ppx
+    def calc_ppx_change(self) -> torch.tensor:
+        ppx_1d_list = []
+        ppx_2d_list = []
+        for src_lang in self.lang_list:
+            src_dataset = WikipediaDataset(tokenizer=self.tokenizer, lang=src_lang, max_context_len=self.Tmax) 
+            src_ppx = self._calc_ppx_for_lang(dataset=src_dataset, inv_lang=None, lang=src_lang)
+            ppx_1d_list.append(src_ppx)
+            
+            ppx_inv_list = []
+            for tgt_lang in self.lang_list:
+                tgt_dataset = WikipediaDataset(tokenizer=self.tokenizer, lang=tgt_lang, max_context_len=self.Tmax)
+                tgt_ppx = self._calc_ppx_for_lang(dataset=tgt_dataset, inv_lang=src_lang, lang=tgt_lang)
+                ppx_inv_list.append(tgt_ppx)
+            ppx_2d_list.append(ppx_inv_list)
+            
+        ppx_1d_tensor = torch.tensor(ppx_1d_list)
+        ppx_2d_tensor = torch.tensor(ppx_2d_list)
+        ppx_change = ppx_2d_tensor - ppx_1d_tensor
+        return ppx_change
         
 def main(model_name: str, device: torch.device) -> None:
     tokenizer = AutoTokenizer.from_pretrained(model_name)
@@ -66,17 +87,16 @@ def main(model_name: str, device: torch.device) -> None:
     else:
         raise NotImplementedError("Invalid model name!")
     
-    max_context_len = 512
-    batch_size = 4
-    data_frac = 0.001
-    
-    src_lang = "en"
-    tgt_lang = "en"
-    src_dataset = WikipediaDataset(tokenizer=tokenizer, lang=src_lang, max_context_len=max_context_len)   
-    tgt_dataset = WikipediaDataset(tokenizer=tokenizer, lang=tgt_lang, max_context_len=max_context_len)   
-    ppx = Perplexity(device=device, model=model, model_name=model_name, src_lang_dataset=src_dataset)
-    print(ppx._calc_ppx_for_src_lang(batch_size, data_frac))
-    print(ppx._calc_ppx_for_tgt_lang(tgt_dataset, batch_size, data_frac))
+    ppx_config = {
+        "lang_list": ["en", "fr", "es", "vi", "id", "ja", "zh"],
+        "max_context_len": 512,
+        "batch_size": 4,
+        "data_frac": 0.01
+    }
+     
+    ppx = Perplexity(device=device, tokenizer=tokenizer, model=model, model_name=model_name, ppx_config=ppx_config)
+    out = ppx.calc_ppx_change()
+    print(out)
     
 if __name__ == "__main__":
     os.environ["CUDA_VISIBLE_DEVICES"] = "6"
