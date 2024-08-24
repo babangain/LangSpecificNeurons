7ab132dc731d2a4198904511dd943b731d86d736
Soumen Mondal <mondalsoumen00@gmail.com>
2024-08-24 12:39:03 +0530

    Add results for model set 2

--- src/models.py
+++ src/models.py
@@ -1,11 +1,26 @@
 import torch, json, os, sys
 from pathlib import Path
 sys.path.append(Path(__file__).parent)
-from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import AutoTokenizer, AutoModelForCausalLM, AutoModelForSeq2SeqLM
 from typing import List, Tuple, Union
 import pandas as pd
 from dataset import WikipediaDataset
 from abc import ABC, abstractmethod
+
+models_dict = {
+    "llama2-pt": "meta-llama/Llama-2-7b-hf", # Done
+    "llama2-ft": "meta-llama/Llama-2-7b-chat-hf", # Done
+    "llama3-pt": "meta-llama/Meta-Llama-3.1-8B", # Done
+    "llama3-ft": "meta-llama/Meta-Llama-3.1-8B-Instruct", # Done
+    "mistral-pt": "mistralai/Mistral-7B-v0.3", # Done
+    "mistral-ft": "mistralai/Mistral-7B-Instruct-v0.3", # Done
+    "bloomz-pt": "bigscience/bloomz-7b1", # Done
+    "bloomz-mt-pt": "bigscience/bloomz-7b1-mt", # Done
+    "bloom-pt": "bigscience/bloom-7b1", # Done
+    "sarvam-pt": "sarvamai/sarvam-2b-v0.5", # Done
+    "aya101-ft": "CohereForAI/aya-101",
+    "aya23-ft": "CohereForAI/aya-23-8B"
+}
 
 class AbstractModelForProbing(ABC, torch.nn.Module):
     def __init__(self, device: torch.device, model_name: str, model: torch.nn.Module, tokenizer: Union[AutoTokenizer, None]):
@@ -15,6 +30,7 @@ class AbstractModelForProbing(ABC, torch.nn.Module):
         self.model_name = model_name.split("/")[-1]
         self.model = model.to(self.device)
         self.tokenizer = tokenizer
+        self.model_type = "ed" if "aya-101" in model_name.lower() else "d"
 
     @abstractmethod
     def get_layers(self) -> torch.nn.ModuleList:
@@ -62,7 +78,13 @@ class AbstractModelForProbing(ABC, torch.nn.Module):
         self.model.eval()
         with torch.no_grad():
             self.register_hook(intervene_config=intervene_config)
-            prediction_output = self.model(input_ids=input_ids, attention_mask=attention_mask).logits.cpu() # (b, T, d)
+            if self.model_type == "ed":
+                decoder_input_ids = input_ids[:, :-1]  # Shifting input IDs by one 
+                prediction_output = self.model(input_ids=input_ids, attention_mask=attention_mask, decoder_input_ids=decoder_input_ids).logits.cpu() # (b, T, d)
+            elif self.model_type == "d":
+                prediction_output = self.model(input_ids=input_ids, attention_mask=attention_mask).logits.cpu() # (b, T, d)
+            else:
+                raise ValueError("Sorry! Invalid model type")
             self.remove_hook()
         
         neuron_output = torch.stack(self.neuron_output_list, dim=0) # (L, b, T, 4d)
@@ -88,16 +110,60 @@ class LlamaModelForProbing(AbstractModelForProbing):
     def get_target_module(self, layer_idx: int) -> torch.nn.Module:
         return self.get_layers()[layer_idx].mlp.act_fn
 
-class BloomzModelForProbing(AbstractModelForProbing):
+class BloomModelForProbing(AbstractModelForProbing):
     def __init__(self, device: torch.device, model_name: str, tokenizer: Union[AutoTokenizer, None]):
         model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=torch.bfloat16)
-        super(BloomzModelForProbing, self).__init__(device=device, model_name=model_name, model=model, tokenizer=tokenizer)
+        super(BloomModelForProbing, self).__init__(device=device, model_name=model_name, model=model, tokenizer=tokenizer)
     
     def get_layers(self) -> torch.nn.ModuleList:
         return self.model.transformer.h
     
     def get_target_module(self, layer_idx: int) -> torch.nn.Module:
         return self.get_layers()[layer_idx].mlp.gelu_impl
+
+class MistralModelForProbing(AbstractModelForProbing):
+    def __init__(self, device: torch.device, model_name: str, tokenizer: Union[AutoTokenizer, None]):
+        model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=torch.bfloat16)
+        super(MistralModelForProbing, self).__init__(device=device, model_name=model_name, model=model, tokenizer=tokenizer)
+    
+    def get_layers(self) -> torch.nn.ModuleList:
+        return self.model.model.layers
+    
+    def get_target_module(self, layer_idx: int) -> torch.nn.Module:
+        return self.get_layers()[layer_idx].mlp.act_fn
+
+class SarvamModelForProbing(AbstractModelForProbing):
+    def __init__(self, device: torch.device, model_name: str, tokenizer: Union[AutoTokenizer, None]):
+        model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=torch.bfloat16)
+        super(SarvamModelForProbing, self).__init__(device=device, model_name=model_name, model=model, tokenizer=tokenizer)
+    
+    def get_layers(self) -> torch.nn.ModuleList:
+        return self.model.model.layers
+    
+    def get_target_module(self, layer_idx: int) -> torch.nn.Module:
+        return self.get_layers()[layer_idx].mlp.act_fn
+
+class AyaEncDecModelForProbing(AbstractModelForProbing):
+    def __init__(self, device: torch.device, model_name: str, tokenizer: Union[AutoTokenizer, None]):
+        model = AutoModelForSeq2SeqLM.from_pretrained(model_name, torch_dtype=torch.bfloat16)
+        super(AyaEncDecModelForProbing, self).__init__(device=device, model_name=model_name, model=model, tokenizer=tokenizer)
+    
+    def get_layers(self) -> torch.nn.ModuleList:
+        return self.model.decoder.block
+    
+    def get_target_module(self, layer_idx: int) -> torch.nn.Module:
+        return self.get_layers()[layer_idx].layer[2].DenseReluDense.act
+
+class AyaDecModelForProbing(AbstractModelForProbing):
+    def __init__(self, device: torch.device, model_name: str, tokenizer: Union[AutoTokenizer, None]):
+        model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=torch.bfloat16)
+        super(AyaDecModelForProbing, self).__init__(device=device, model_name=model_name, model=model, tokenizer=tokenizer)
+    
+    def get_layers(self) -> torch.nn.ModuleList:
+        return self.model.model.layers
+    
+    def get_target_module(self, layer_idx: int) -> torch.nn.Module:
+        return self.get_layers()[layer_idx].mlp.act_fn
 
 def get_tokenizer_and_model(model_name: Union[str, None], device: torch.device) -> Tuple[AutoTokenizer, torch.nn.Module]:
     if model_name is None:
@@ -107,8 +173,16 @@ def get_tokenizer_and_model(model_name: Union[str, None], device: torch.device) 
         tokenizer = AutoTokenizer.from_pretrained(model_name)
         if "llama" in model_name.lower():
             model = LlamaModelForProbing(tokenizer=tokenizer, device=device, model_name=model_name)
-        elif "bloomz" in model_name.lower():
-            model = BloomzModelForProbing(tokenizer=tokenizer, device=device, model_name=model_name)
+        elif "bloom" in model_name.lower():
+            model = BloomModelForProbing(tokenizer=tokenizer, device=device, model_name=model_name)
+        elif "mistral-7b" in model_name.lower():
+            model = MistralModelForProbing(tokenizer=tokenizer, device=device, model_name=model_name)
+        elif "sarvam" in model_name.lower():
+            model = SarvamModelForProbing(tokenizer=tokenizer, device=device, model_name=model_name)
+        elif "aya-101" in model_name.lower():
+            model = AyaEncDecModelForProbing(tokenizer=tokenizer, device=device, model_name=model_name)
+        elif "aya-23" in model_name.lower():
+            model = AyaDecModelForProbing(tokenizer=tokenizer, device=device, model_name=model_name)
         else:
             raise NotImplementedError("Invalid model name!")
     return tokenizer, model
@@ -123,17 +197,17 @@ def main(model_name: str, device: torch.device) -> None:
         "indices": list(zip([1]*(n-1), range(n))),
         "value": 0
     }
+    print(model)
     out = model(input_dict["input_ids"], input_dict["attention_mask"], intervene_config=intervene_config)
     print(out["logits"].sum())      
     out = model(input_dict["input_ids"], input_dict["attention_mask"], intervene_config=None)
     print(out["logits"].sum())  
     
 if __name__ == "__main__":
-    os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+    os.environ["CUDA_VISIBLE_DEVICES"] = "3"
     torch.cuda.empty_cache()
-    models = ["meta-llama/Llama-2-7b-hf", "bigscience/bloomz-7b1"]
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f"Using {device}...")
     
-    main(models[1], device=device)
+    main(models_dict["bloomz-mt-pt"], device=device)
     
